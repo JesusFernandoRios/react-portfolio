f2eab723add5d79a41608509f32deba4646dcacc
jesus <Jesusfernandorios93@gmail.com>
2020-10-16 01:18:38 -0700

    worked on contacts page, setting up mobile responsive

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -11,18 +11,18 @@ function ContactPage() {
                 <br/>
                 <h2>Thank you for taking the time to contact me!</h2>
                 <form action="https://submit-form.com/QL2cL0nQU3o5v4PPw3bMW" target="_self">
-                    <div className="form__container">
+                    {/* <div className="form__container"> */}
 
                         <h3>E-mail</h3>
                         <input type="text" name="email" placeholder="Email"/>
                         <h3>Subject</h3>
                         <input type="text" name="subject" placeholder="Subject"/>
                         <h3>Message</h3>
-                        <textarea name="message" placeholder="Message" rows='8' cols='60'></textarea>
+                        <textarea name="message" placeholder="Message"></textarea>
                         <br/>
                         {/* <!-- A button of type submit --> */}
                         <button type="submit">Submit</button>
-                    </div>
+                    {/* </div> */}
                     
                 </form>
             </div>
