ddc2bef0c69342ad878245f73b71e55a9ac9878e
jesus <Jesusfernandorios93@gmail.com>
2020-11-23 18:27:50 -0700

    made changes to main summary, updated resume and added favicon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,9 +18,9 @@ function Main() {
                     <p>
                         I've worked my way up in the Restaurant Industry, which lead me to become Head Sushi Chef at a local Japanese restaurant. 
                         Although this has been fulfilling, I've always been drawn towards my hobbies involving computers. 
-                        This has pushed me to decide to take a career change and start a new chapter in my life. I am currently enrolled at the University of Arizona's Programming Bootcamp.
+                        This has pushed me to decide to take a career change and start a new chapter in my life. Having earned my certification from the University of Arizona's Programming Bootcamp.
                         I am eager to refine and evolve my skill set and become more established as a business professional in this field. 
-                        My goal after this bootcamp is to retire my Yanagiba knife and to dive deep into my new career as a Web Developer.
+                        My goal is to retire my Yanagiba knife and to dive deep into my new career as a Web Developer.
                     </p>
                 
                     <p>
