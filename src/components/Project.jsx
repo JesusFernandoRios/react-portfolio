f76d78c96bc2494ca00c23f0df08bbfaf3af72eb
jesus <Jesusfernandorios93@gmail.com>
2021-01-05 16:11:30 -0700

    updated my portfolio page. Modified my projects and added covid tracker app to portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Project({image, desc, repo}) {
+function Project({image, desc, repo, live}) {
     return (
         <div className='project'>
 
@@ -9,6 +9,8 @@ function Project({image, desc, repo}) {
             <p>{desc}</p>
 
             <a href={repo} title="Repository" target="_blank" className="fa fa-github repo" rel="noopener noreferrer"></a>
+
+            <a href={live} title="Live Application" target="_blank" className="fa fa-window-maximize"></a>
             
         </div>
     )
--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -17,21 +17,24 @@ function Projects() {
 
             <div className="project__content">
                 <Project 
-                image={require('../img/codequiz.png')}
-                desc={'This application contains Javascript fundamental questions designed to test the knowledge of aspiring web developers'}
-                repo={"https://jesusfernandorios.github.io/codequiz/"}
+                image={require('../img/covid.png')}
+                desc={'I Created this Application to share with my friends and family so they can be updated on covid information regarging total cases, cases by country, recoveries and deaths. '}
+                repo={"https://github.com/JesusFernandoRios/Covid-19-tracker"}
+                live={"https://jesusfernandorios.github.io/Covid-19-tracker/"}
                 />
 
                 <Project 
                 image={require('../img/Dayplanner.png')}
                 desc={'The Day planner is a simple application designed for setting reminders throughout the work day.'}
                 repo={"https://github.com/JesusFernandoRios/DayPlanner.git"}
+                live={"https://jesusfernandorios.github.io/DayPlanner/"}
                 />
 
                 <Project 
                 image={require('../img/googlebksrch.png')}
                 desc={"Search and save book titles using my API. It Takes book information from google books and lets you save them for later"}
                 repo={"https://github.com/JesusFernandoRios/googleBookSearch.git"}
+                live={"https://sleepy-hollows.herokuapp.com/"}
                 />
                 
             </div>
@@ -41,18 +44,21 @@ function Projects() {
                 image={require('../img/notetaker.png')}
                 desc={'Keep track of your notes with this application! Stores all your notes on a JSON file'}
                 repo={"https://github.com/JesusFernandoRios/NoteTaker.git"}
+                live={"https://desolate-beyond-54031.herokuapp.com/"}
                 />
 
                 <Project 
                 image={require('../img/burger.png')}
                 desc={'Users can create hamburgers and devour them. This application uses MySQL, handlebars and express.js'}
                 repo={"https://github.com/JesusFernandoRios/Burger.git"}
+                live={"https://stark-hollows-63908.herokuapp.com/index"}
                 />
 
                 <Project 
-                image={require('../img/weather-app.png')}
-                desc={'This application gets weather information for the next 5 days of any specific City '}
-                repo={"https://github.com/JesusFernandoRios/WeatherApp.git"}
+                image={require('../img/codequiz.png')}
+                desc={'This application contains Javascript fundamental questions designed to test the knowledge of aspiring web developers'}
+                repo={"https://github.com/JesusFernandoRios/codequiz"}
+                live={"https://jesusfernandorios.github.io/codequiz/"}
                 />
             </div>
 
